81d60c03d9b397a056d91cd57cdf39e7d499221d
Aaron <apeckham@gmail.com>
2024-06-21 06:15:42 +0000

    simplify fix

--- assertis/cmd_fix.py
+++ assertis/cmd_fix.py
@@ -15,19 +15,17 @@ from assertis.models import (
 
 def apply_changes(report, expected, dry_run):
     for file in report.files:
+        target_path = Path(expected) / file.name
+        target_path.parent.mkdir(parents=True, exist_ok=True)
         if isinstance(file, AddedFile):
-            target_path = Path(expected) / file.name
-            target_path.parent.mkdir(parents=True, exist_ok=True)
             if not dry_run:
                 shutil.copy(file.path_src_actual, target_path)
             print(f"{'Would add' if dry_run else 'Added'} file {target_path}")
         elif isinstance(file, DeletedFile):
-            target_path = Path(expected) / file.name
             if not dry_run:
                 target_path.unlink()
             print(f"{'Would delete' if dry_run else 'Deleted'} file {target_path}")
         elif isinstance(file, ChangedFile):
-            target_path = Path(expected) / file.name
             if not dry_run:
                 shutil.copy(file.path_src_actual, target_path)
             print(f"{'Would change' if dry_run else 'Changed'} file {target_path}")
